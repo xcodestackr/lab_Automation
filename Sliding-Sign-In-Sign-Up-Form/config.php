38184651265b6b2be953360ba7706275f97daf48
Abdul Majeed (Xcode Stackr) <114279240+xcodestackr@users.noreply.github.com>
2023-02-13 10:40:18 +0500

    Add files via upload

---
+++ Sliding-Sign-In-Sign-Up-Form/config.php
@@ -0,0 +1,10 @@
+<?php
+
+    $hostname = 'localhost';
+    $user = 'root';
+    $pass = '';
+    $db = 'labAutomation';
+
+    $conm = mysqli_connect($hostname,$user,$pass,$db) or die("database connection failed");
+
+?>
